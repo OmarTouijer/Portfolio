6144948b535b217f4484717f8e693fc988619d46
Omar Touijer <omar.touiejr2004@gmail.com>
2026-06-07 00:32:43 -0400

    link fresh hair studio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
       description: "Website created for a Hairdresser shop.",
       image:
         "https://images.unsplash.com/photo-1562322140-8baeececf3df?q=80&w=1738&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      link: "/coming-soon",
+      link: "https://freshhairstudio.netlify.app/",
     },
     {
       id: 2,
